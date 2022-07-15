680f4b9dd8cf3abecde5e2b00287302077df7112
zerbinidamata <rafael.zerbinia@gmail.com>
2022-07-15 10:55:28 -0300

    (feat): order and product dto + basic payment creation model

--- payment/src/app.module.ts
+++ payment/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CreditCardController } from './creditCard/creditCard.controller';
 import { CreditCardService } from './creditCard/creditCard.service';
+import { PaymentService } from './payment/payment.service';
 import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
   controllers: [CreditCardController],
-  providers: [CreditCardService, PrismaService],
+  providers: [CreditCardService, PaymentService, PrismaService],
 })
 export class AppModule {}
---
+++ payment/src/payment/payment.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { Payment, Prisma } from '@prisma/client';
+
+@Injectable()
+export class PaymentService {
+  constructor(private prisma: PrismaService) {}
+
+  async createPayment(data: Prisma.PaymentCreateInput): Promise<Payment> {
+    return this.prisma.payment.create({
+      data,
+    });
+  }
+}
